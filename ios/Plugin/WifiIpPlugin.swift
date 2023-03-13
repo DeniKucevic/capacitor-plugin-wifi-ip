9892174f08b6bd5d21d2e84d6dfb65f2b0ee0925
brixi <denikucevic@gmail.com>
2023-03-13 11:29:25 +0100

    update ios

--- ios/Plugin/WifiIpPlugin.swift
+++ ios/Plugin/WifiIpPlugin.swift
@@ -15,41 +15,44 @@ public class WifiIpPlugin: CAPPlugin {
             call.reject("ERROR_NO_WIFI_IP_AVAILABLE");
             return
         } 
-        call.success([
+        call.resolve([
             "ip": address!
         ])
     }
     
+// Return IP address of WiFi interface (en0) as a String, or `nil`
      @objc func getWiFiAddress() -> String? {
         var address : String?
 
-        // Get list of all interfaces on the local machine:
-        var ifaddr : UnsafeMutablePointer<ifaddrs>?
-        guard getifaddrs(&ifaddr) == 0 else { return nil }
-        guard let firstAddr = ifaddr else { return nil }
+    // Get list of all interfaces on the local machine:
+    var ifaddr : UnsafeMutablePointer<ifaddrs>?
+    guard getifaddrs(&ifaddr) == 0 else { return nil }
+    guard let firstAddr = ifaddr else { return nil }
 
-        // For each interface ...
-        for ifptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
-            let interface = ifptr.pointee
+    // For each interface ...
+    for ifptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
+        let interface = ifptr.pointee
 
-            // Check for IPv4:
-            let addrFamily = interface.ifa_addr.pointee.sa_family
-            // addrFamily == UInt8(AF_INET6)
-            if addrFamily == UInt8(AF_INET) {
-                // Check interface name:
-                let name = String(cString: interface.ifa_name)
-                if  name == "en0" || name.starts(with: "tap") || name.starts(with: "ppp") || name.starts(with: "ipsec") || name.starts(with: "utun") {
-                    // Convert interface address to a human readable string:
-                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
-                    getnameinfo(interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len),
-                                &hostname, socklen_t(hostname.count),
-                                nil, socklen_t(0), NI_NUMERICHOST)
-                    address = String(cString: hostname)
-                }
+        // Check for IPv4 or IPv6 interface:
+        let addrFamily = interface.ifa_addr.pointee.sa_family
+        if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+
+            // Check interface name:
+            let name = String(cString: interface.ifa_name)
+            if  name == "en0" {
+
+                // Convert interface address to a human readable string:
+                var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                getnameinfo(interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len),
+                            &hostname, socklen_t(hostname.count),
+                            nil, socklen_t(0), NI_NUMERICHOST)
+                address = String(cString: hostname)
             }
         }
-        freeifaddrs(ifaddr)
+    }
+    freeifaddrs(ifaddr)
 
-        return address
+    return address
+
     }
 }
